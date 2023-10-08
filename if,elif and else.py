ad3b493e381eac475120f303da7d15d574a58266
divyarathore07 <145034432+divyarathore07@users.noreply.github.com>
2023-10-08 02:40:57 -0700

    Add files via upload

---
+++ if,elif and else.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  4 21:51:13 2023
+
+@author: USER
+"""
+
+a=0
+if a>0:
+    print('positive')
+elif a==0:
+    print('zero')
+else:
+    print('negative')    
