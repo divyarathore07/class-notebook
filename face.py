026825d95364bb47b0cc2406b5c58d94dbc00b55
divyarathore07 <145034432+divyarathore07@users.noreply.github.com>
2023-10-08 02:32:26 -0700

    Add files via upload

---
+++ face.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 20:14:14 2023
+
+@author: USER
+"""
+import turtle
+t=turtle.Turtle()
+t.circle(100,360)
+t.penup()
+t.goto(-40,120)
+t.pendown()
+t.circle(15,360)
+t.penup()
+t.goto(40,120)
+t.pendown()
+t.circle(15,360)
+t.penup()
+t.goto(40,80)
+t.pendown()
+t.right(180)
+t.forward(80)
+t.left(90)
+t.circle(40,180)
+      
